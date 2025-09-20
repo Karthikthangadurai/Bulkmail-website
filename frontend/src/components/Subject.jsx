09cc8bf7fd7e0ad56e5b8f73c30cabd61f7f1258
Karthikthangadurai <presikarthik@gmail.com>
2025-09-20 21:47:35 +0530

    screen view

--- frontend/src/components/Subject.jsx
+++ frontend/src/components/Subject.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Subject = () => {
   return (
-       <div className="bg-blue-800 text-white text-center text-xs md:text-base">
+       <div className="bg-blue-800 text-white text-center text-xs md:text-lg">
         <h1 className="font-semibold px-5 py-4">We can help your business with sending multiple emails at once easy way📨</h1>
   </div>
   );
